1a8f5f257302740fbed38738a758d552bb712ab1
Shreya Sarkar <94786437+shreyasarkar28@users.noreply.github.com>
2022-05-25 20:00:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./components/Board";
+// import Square from "./components/square";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">tic-tac-toe</h1>
+      <Board/>
+  
+    </div>
+  );
+}
+
+export default App;
